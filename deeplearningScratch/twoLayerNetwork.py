7e7fb5f89de7bd496dc53dc358b5abb62ddd93dd
crexy <crexy@daum.net>
2022-09-23 00:18:27 +0900

    * twoLayerNetwork.py   - 오차역전파법을 사용한 학습 구현 * numpy_practice.ipynb   - random.choice를 이용한 마스킹 방법 추가

--- deeplearningScratch/twoLayerNetwork.py
+++ deeplearningScratch/twoLayerNetwork.py
@@ -2,6 +2,7 @@ from typing import OrderedDict
 import numpy as np
 from deeplearning_common_func import DLCFunc
 from dataset.mnist import load_mnist
+import matplotlib.pyplot as plt
 
 
 # Relu 활성화 함수 레이어
@@ -183,3 +184,42 @@ if __name__ == "__main__":
     #     print(f'Key({key}): {diff}')
 
     # 학습 구현
+    # 배치 사이즈: 100, 에포크: 1000, 학습방법: 경사하강법, learning rate: 0.01
+
+    batch_size = 100
+    epoch = 1000
+    learning_rate = 1e-1
+    list_loss = [] # 손실값 리스트
+
+    for ep in range(epoch):
+        mask = np.random.choice(x_train.shape[0], batch_size) # 배치 마스크
+        # 미니배치 데이터
+        x_batch = x_train[mask]
+        t_batch = t_train[mask]
+
+        # 미분값 구하기
+        grad = network.gradient(x_batch, t_batch)
+
+        # 경사하강법 학습 수행
+        for key in grad.keys():
+            network.params[key] -= grad[key]*learning_rate
+
+        # 손실값
+        loss = network.loss(x_batch, t_batch)
+        list_loss.append(loss) # 손실값 그래프를 그리기 위해 저장
+        # 정확도
+        accuracy = network.accuracy(x_batch, t_batch)
+        if (ep % 10) == 0:
+            print(f'epoch: {ep+1}, loss: {loss}, accuracy: {accuracy}')
+    # 손실값 그래프 그리기
+    plt.figure(figsize=(10,6))
+    plt.title("Loss value")
+    plt.ylabel("loss")
+    plt.xlabel("epoch")
+    plt.plot(list_loss)
+    plt.show()
+
+
+
+
+
