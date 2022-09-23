f0b814c241a827a74175103d1bad37eb959dc85d
crexy <crexy@daum.net>
2022-09-24 01:59:07 +0900

    옵티마이저 코드 추가

--- deeplearningScratch/twoLayerNetwork.py
+++ deeplearningScratch/twoLayerNetwork.py
@@ -167,6 +167,119 @@ class TwoLayerNet:
 
         return grads
 
+# 확률적 경사하강법 옵티마이저 클래스
+class SGD:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+    def update(self, param, grad):
+        for key in param.keys():
+            param[key] -= grad[key]*self.lr
+
+# 모멘텀 옵티마이저 클래스
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+    def update(self, param, grad):
+        if self.v == None:
+            self.v = {}
+            for key, val in param.items():
+                self.v[key] = np.zeros_like(val)
+        for key in param.keys():
+            self.v[key] = self.momentum*self.v[key] - (self.lr*grad[key])
+            param[key] += self.v[key]
+
+class AdaGrad:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+    def update(self, params, grad):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+        for key in params.keys():
+            self.h[key] += grad[key]*grad[key]
+            params[key] -= self.lr*(1/ (np.sqrt(self.h[key])+1e-7))*grad[key]
+            #params[key] -= self.lr * grad[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
+class Adam:
+    """Adam (http://arxiv.org/abs/1412.6980v8)"""
+
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+
+        self.iter += 1
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2 ** self.iter) / (1.0 - self.beta1 ** self.iter)
+
+        for key in params.keys():
+            # self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
+            # self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
+            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+            self.v[key] += (1 - self.beta2) * (grads[key] ** 2 - self.v[key])
+
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
+
+            # unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
+            # unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
+            # params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
+
+
+class RMSprop:
+    """RMSprop"""
+
+    def __init__(self, lr=0.01, decay_rate=0.99):
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] *= self.decay_rate
+            self.h[key] += (1 - self.decay_rate) * grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
+class Nesterov:
+    """Nesterov's Accelerated Gradient (http://arxiv.org/abs/1212.0901)"""
+
+    # NAG는 모멘텀에서 한 단계 발전한 방법이다. (http://newsight.tistory.com/224)
+
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] *= self.momentum
+            self.v[key] -= self.lr * grads[key]
+            params[key] += self.momentum * self.momentum * self.v[key]
+            params[key] -= (1 + self.momentum) * self.lr * grads[key]
 
 if __name__ == "__main__":
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
@@ -191,6 +304,9 @@ if __name__ == "__main__":
     learning_rate = 1e-1
     list_loss = [] # 손실값 리스트
 
+    optimizer = Nesterov() # 가중치 값 최적화 수행
+
+
     for ep in range(epoch):
         mask = np.random.choice(x_train.shape[0], batch_size) # 배치 마스크
         # 미니배치 데이터
@@ -201,8 +317,7 @@ if __name__ == "__main__":
         grad = network.gradient(x_batch, t_batch)
 
         # 최적화(경사하강법) 수행
-        for key in grad.keys():
-            network.params[key] -= grad[key]*learning_rate
+        optimizer.update(network.params, grad)
 
         # 손실값
         loss = network.loss(x_batch, t_batch)
