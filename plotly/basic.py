0b6005b8b719e776d84a612a2d91db937b298ed7
crexy <crexy@daum.net>
2022-12-20 22:34:38 +0900

    plotly 추가

---
+++ plotly/basic.py
@@ -0,0 +1,8 @@
+import plotly.express as px
+
+df = px.data.gapminder().query("country=='Canada'")
+fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+fig.show()
+
+if __name__ == "__main__":
+    pass
